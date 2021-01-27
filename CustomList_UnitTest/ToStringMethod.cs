5c8fbde8fe060cf20a8856b1326d539e3bb81814
Nick Johnson <johnsn0913@gmail.com>
2021-01-27 11:10:58 -0600

    ToString Tests for converting Integers created

---
+++ CustomList_UnitTest/ToStringMethod.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomList_UnitTest
+{
+    [TestClass]
+    public class ToStringMethod
+    {
+        [TestMethod]
+        public void ConvertIntToStringInCustomListAtIndex1()
+        {
+            //Arrange
+            TheCustomList<int> list = new TheCustomList<int>();
+            int firstNumber = 1;
+            int secondNumber = 2;
+            string actual;
+            string expected = "2";
+
+            //Act
+            list.Add(firstNumber);
+            list.Add(secondNumber);
+            actual = list.ToString(list[1]);
+            
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ConvertIntToStringInCustomList_MultipleInts()
+        {
+            //Arrange
+            TheCustomList<int> list = new TheCustomList<int>();
+            int firstNumber = 1;
+            int secondNumber = 2;
+            string actual;
+            string expected = "21";
+
+            //Act
+            list.Add(firstNumber);
+            list.Add(secondNumber);
+            actual = list.ToString(list[1]+list[0]);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
